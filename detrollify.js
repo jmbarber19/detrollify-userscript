518dc82110a94aecaefd3c9b06e4caa8f438a7b1
Jeff Wax <jeff@jeffwax.com>
2018-09-17 11:09:47 -0700

    Add basic manifest, icon, and initial script.

---
+++ detrollify.js
@@ -0,0 +1,11 @@
+/**
+ detrollify.js by @jwaxo
+ This seeks out the chat logs on a page of Homestuck and attempts to remove the
+ various typing "quirks" of Trolls to make them more legible.
+
+ As typing quirks are considered a large part of a troll's personality, and may
+ have some story implications, this can be seen as a hostile move by the
+ Homestuck fandom. You have been warned.
+ **/
+
+
